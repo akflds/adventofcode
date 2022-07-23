fbf3d16569d15b66e1019899db5c1c4a0f90f880
akflds <andrewkirsfelds@gmail.com>
2022-07-23 20:09:29 +0100

    2017 day 2 p2

--- 2017/2.py
+++ 2017/2.py
@@ -4,9 +4,16 @@ with open('2.txt') as f:
     data = f.readlines()
 
 total = 0
+div_total = 0
 
 for d in data:
   d = [int(x) for x in d.strip().split("\t")]
   total += max(d)-min(d)
-
-print(total)
+  d.sort(reverse=True)
+  for i in range(0, len(d)):
+    for j in range(i + 1, len(d)):
+      if d[i] % d[j] == 0:
+        div_total += int(d[i] / d[j])
+  
+print("p1:", total)
+print("p2:", div_total)
